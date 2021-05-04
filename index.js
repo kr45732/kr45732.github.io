a63dec82e3d929b9c6a239a8180f7f8893efb27a
kr45732 <52721908+kr45732@users.noreply.github.com>
2021-05-03 21:18:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/home', (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index1.html");
 });
 
 app.get('/projects', (req, res) => {
