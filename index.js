afd29926ab747c75783162bd1906e09198442c0d
kr45732 <52721908+kr45732@users.noreply.github.com>
2021-05-03 21:22:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/home.html");
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + "/home.html");
+// });
 
-app.get('/projects', (req, res) => {
-  res.sendFile(__dirname + "/projects.html");
-});
+// app.get('/projects', (req, res) => {
+//   res.sendFile(__dirname + "/projects.html");
+// });
 
-app.listen(3000, () => {
-  console.log('server started');
-});
+// app.listen(3000, () => {
+//   console.log('server started');
+// });
